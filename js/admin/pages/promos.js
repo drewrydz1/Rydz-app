fbed10561cb0ff27300fcaf2856503a86d8626d7
drewrydz1 <drew@riderydz.com>
2026-03-28 00:53:30 -0400

    Update promos.js

--- js/admin/pages/promos.js
+++ js/admin/pages/promos.js
@@ -1,3 +1,3 @@
-// RYDZ Admin - Promotions
+// RYDZ Admin - Promos Page
 
 function renderPromos(){var el=document.getElementById('promo-list');el.innerHTML='<div style="color:var(--tx3);font-size:13px;padding:20px">Promotions CMS coming soon.</div>'}
