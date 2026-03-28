2b26ec853c4318267a8ea0779093ba07acf7139d
drewrydz1 <drew@riderydz.com>
2026-03-27 22:50:14 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+// RYDZ Shared - Configuration
+// Single source of truth for all credentials and constants
+// Change here = changes everywhere
+
+var SUPA_URL = 'https://ewnynyazfkcyqakyuzcd.supabase.co';
+var SUPA_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV3bnlueWF6ZmtjeXFha3l1emNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM5NDQzNDIsImV4cCI6MjA4OTUyMDM0Mn0.Ns0do2aYhXfsi4SS_mfaJvuMy6caJNIYgUE_kxqkZ9c';
+var SUPA_H = {
+  'apikey': SUPA_KEY,
+  'Authorization': 'Bearer ' + SUPA_KEY,
+  'Content-Type': 'application/json',
+  'Prefer': 'return=representation'
+};
+
+var GOOGLE_MAPS_KEY = 'AIzaSyDvV2iMkLWP5twK_EyLC4L-Hjnp1Xsrkdw';
