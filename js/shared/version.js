a770b6fcfe00375ae51e665188730d26a6472fe7
drewrydz1 <drew@riderydz.com>
2026-03-27 22:53:13 -0400

    Update version.js

--- js/shared/version.js
+++ js/shared/version.js
@@ -1,1 +1,8 @@
+// RYDZ Shared - Version Tracking
+// Update this on every deploy
 
+var RYDZ_VERSION = '5.1.0';
+var RYDZ_BUILD = '2026-03-27';
+var RYDZ_ENV = 'production';
+
+console.log('RYDZ v' + RYDZ_VERSION + ' (' + RYDZ_BUILD + ') [' + RYDZ_ENV + ']');
